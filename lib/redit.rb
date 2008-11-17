d5275b77426f8af179b58cc8ba91eb8905d4ffb9
Steven Dee <steve@smartercode.net>
2008-11-17 10:44:38 -0500

    only check for nullity once; also, print a ? if you're dumb

--- lib/redit.rb
+++ lib/redit.rb
@@ -23,8 +23,12 @@ class Redit
         /^([0-9])*(#{CMD_REGEX})(.*)$/.match str
         cmd,args = $2,$3
         count = ($1 or 1).to_i
-        count.times do
-            @exiting = @command.send cmd, args unless cmd.nil?
+        if cmd.nil?
+            puts '?'
+        else
+            count.times do
+                @exiting = @command.send cmd, args
+            end
         end
     end
 
